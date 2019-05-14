90607a761a7caadb3e42f24689cd0efe355b04d6
Gaston Rivarola <daxtergaston@hotmail.com>
2019-05-13 22:51:53 -0300

    Se elimina referencia al archivo faltante Crane.cs.
    Se agrega version inicial del proyecto de Abstracciones.

---
+++ GameProgra2/Abstracciones/EntidadBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstracciones
+{
+    /// <summary>
+    /// Clase base que contiene las propiedades que todos los objetos que 
+    /// van a mostrarse graficamente comparten.
+    /// </summary>
+    public abstract class EntidadBase
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        protected string Sprite { get; set; }
+        
+    }
+}
---
+++ GameProgra2/Abstracciones/IGestorVisualizador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstracciones
+{
+    public interface IGestorVisualizador
+    {
+        /// <summary>
+        /// Me posiciono en el nodo menor que depende del nodo en el que
+        /// estamos actualmente.
+        /// </summary>
+        void AccederNodoMenor();
+
+        /// <summary>
+        /// Me posiciono en el nodo mayor que depende del nodo en el que
+        /// estamos actualmente.
+        /// </summary>
+        void AccederNodoMayor();
+
+        /// <summary>
+        /// Me posiciono en el nodo padre del que depende en el que estamos
+        /// actualmente.
+        /// </summary>
+        void RetornoAPosicionAnterior();
+    }
+}
