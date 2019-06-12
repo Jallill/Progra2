552d11571dcf411d27b92f8234699cf8b407adc7
JALIL PUTER <jputer@uade.edu.ar>
2019-06-12 20:12:32 -0300

    Implementación ABB

---
+++ GameProgra2/Abstractions/Interfaces/IABBTDA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Interfaces
+{
+    public interface IABBTDA
+    {
+        int Raiz();
+        IABBTDA HijoIzq();
+        IABBTDA HijoDer();
+        bool ArbolVacio();
+        void InicializarArbol();
+        void AgregarElem(int x);
+        void EliminarElem(int x);
+    }
+}
